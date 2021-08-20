0d8b144ec53788b56e44071cec6546c37d544e33
luis <lui11235178@gmail.com>
2021-08-21 06:44:30 +0800

    UPDATE unit test

--- tests/test_implementations/test_sqlalchemy/api_test_async/__init__.py
+++ tests/test_implementations/test_sqlalchemy/api_test_async/__init__.py
@@ -39,8 +39,7 @@ class UntitledTable256(Base):
     __table_args__ = (
         UniqueConstraint('id', 'int4_value', 'float4_value'),
     )
-    id = Column(Integer, primary_key=True, info={'alias_name': 'primary_key'},
-                server_default=text("nextval('untitled_table_256_id_seq'::regclass)"))
+    id = Column(Integer, primary_key=True, info={'alias_name': 'primary_key'})
     primary_key = synonym('id')
     bool_value = Column(Boolean, nullable=False, server_default=text("false"))
     bytea_value = Column(LargeBinary)
